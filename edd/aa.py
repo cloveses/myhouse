cd7241aa181b58d3b11cda298ed959b03bbbe544
djx <cloveses@126.com>
2018-12-21 15:31:34 +0800

    m

--- edd/aa.py
+++ edd/aa.py
@@ -1,21 +1,78 @@
+import re
+
 class program:
-    def __init__(self,s):
+    def __init__(self, s):
         self.progs = s
-        self.labels = []
-        self.parse_stutas = 0
+        self.vars = {}
+        self.blocks = {}
+        self.graphs = {}
+
+    def get_var(self, vars):
+        i = 0
+        while True:
+            s = vars[i]
+            if s.startswith('bool'):
+                position = s.index('=')
+                self.vars[s[:position].replace('bool','').strip()] = eval(s[position+1:].strip().strip(';'))
+                i += 1
+            else:
+                break
+        return i
+
+    # def get_index(self, progs, keys=('if','fi','while','done'), indent=0):
+    #     positions  = []
+    #     for index,prog in enumerate(progs):
+    #         if prog in keys:
+    #             positions.append(index)
+    #     if positions:
+    #         positions = [(p[i],p[i+1]) for p in range(0,len(positions),2)]
+    #     return positions
+
+    def parse(self, progs):
+        pre_link = -1
+        block = []
+        for prog in progs:
+            if ':' in prog:
+                label,stmt = prog[:prog.index(':')],prog[prog.index(':')+1:]
+                if 'if' in prog:
+                    block.append((label,stmt.strip(';')))
+                    self.blocks[block[0][0]] = block[:]
+                    pre_link = block[0][0]
+                    block = []
+
+                elif 'while' in prog:
+                    if block:
+                        self.blocks[block[0][0]] = block[:]
+                        self.graphs[block[0][0]] = label
+                    self.blocks[label] = stmt
+                    pre_link = label
+                    block = []
+
+                else:
+                    block.append((label,stmt.strip(';')))
+
+            else:
+                if block:
+                    self.blocks[block[0][0]] = block[:]
+                    self.graphs[pre_link] = block[0][0]
+                    if prog.strip() == 'done':
+                        self.graphs[block[0][0]] = pre_link
+
+                    block = []
+        if block:
+                self.blocks[block[0][0]] = block[:]
+        print(self.blocks)
+
+
 
     def getCFG(self):
-        pass
         progs = self.progs.split('\n')
-        for line in progs:
-            if line:
-                if self.parse_stutas == 0:
-                    if line == '{':
-                        self.parse_stutas = 1
-                        continue
-                    if line == '}':
-                        break
-
+        progs = [prog.strip() for prog in progs if prog.strip()]
+        
+        fun_start = self.get_var(progs)
+        progs = progs[fun_start+1:]
+        progs = [prog for prog in progs if prog not in  ['main()','{','}'] ]
+        self.parse(progs)
 
     def evaluate(self):
         pass
@@ -31,16 +88,17 @@ main()
 1: x= !y;
 2: z= !x;
 3: if ( (x & y) | (! z) )
-4: y= !y;
-5: pass;
-fi
+4:     y= !y;
+5:     pass;
+   fi
 6: x=!y;
 7: z=!z;
 8: while ( ( x | y) & (a | z) )
 9: a=!y;
 10: y=!z;
-done
+   done
 11: return x;
 }
 """
 p = program(s)
+p.getCFG()
