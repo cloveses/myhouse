3a5f7a99a51267e4c7269d7faa3ac5256d35b51d
djx <1181564186@qq.com>
2019-01-14 19:08:13 +0800

    e

--- ttt/q28.py
+++ ttt/q28.py
@@ -3,12 +3,14 @@
 import pickle, os
 import pypinyin
 
+# 新增通讯录
 def add(books):
     musts = ('姓名', '手机号')
     params = {'姓名':'','手机号':'','QQ号':'','EMAIL':'', '城市':'', '邮编':''}
+    # 输入每个信息项
     for key in params.keys():
         params[key] = input(key+':')
-
+    # 检查必须项是否为空
     for must in musts:
         if must not in params:
             print(must, '不能为空！')
@@ -18,20 +20,24 @@ def add(books):
     del params['姓名']
     books[name] = params
 
+# 编辑通讯录（根据姓名）
 def edit(books):
     params = {'手机号':'','QQ号':'','EMAIL':'', '城市':'', '邮编':''}
     name = input('被修改联系人的姓名：')
     if not name or name not in books:
         print('姓名不能为空，或通讯录中不存在！')
         return
+    # 逐项逐项输入
     for key in params.keys():
         params[key] = input(key+':')
+    # 去除输入为空的项
     params = {k:v for k,v in params.items() if v}
     for k,v in params.items():
         books[name][k] = v
     print(books[name])
     print('修改成功!')
 
+# 删除通联系人（根据联系人姓名）
 def del_book(books):
     name = input('被修改联系人的姓名：')
     if not name or name not in books:
@@ -40,17 +46,19 @@ def del_book(books):
     del books[name]
     print('成功删除!')
 
-
+# 搜索通讯录
 def search(books):
     print('按姓名搜？y')
     reply = input()
     if reply.lower() == 'y':
         name = input('被修改联系人的姓名：')
+        # 接受联系人姓名，查找并删除
         if not name or name not in books:
             print('姓名不能为空，或通讯录中不存在！')
             return
         else:
             print(books[name])
+    # 按输入的手机号搜索通讯录
     print('按手机号搜？y')
     reply = input()
     if reply.lower() == 'y':
@@ -63,7 +71,9 @@ def search(books):
                 if number in list(v.values()):
                     print(k,books[k])
 
+# 显示通讯录
 def display(books):
+    # 获取姓名，并转换为拼音后排序
     keys = list(books.keys())
     keys_p = [pypinyin.pinyin(k) for k in keys]
     keys_pp = []
@@ -75,6 +85,7 @@ def display(books):
     # print(keys_pp)
     keys = [(a,b) for a,b in zip(keys,keys_pp)]
     keys.sort(key=lambda s:s[1])
+    # 依据排序的姓名从通讯录中获取并输入
     keys = [k[0] for k in keys]
     for key in keys:
         print(key,books[key])
@@ -82,6 +93,7 @@ def display(books):
 
 def main():
     books = {}
+    # 数据文件存在则读取
     if os.path.exists('q28.db'):
         with open('q28.db', 'rb') as f:
             books = pickle.load(f)
@@ -92,7 +104,7 @@ def main():
     print('编辑联系人－－edit')
     print('查找联系人－－del')
     print('显示联系人－－display')
-
+    # 命令循环，依据输入命令执行相关功能
     while True:
         command = input('请输入命令：')
         if not command:
@@ -108,6 +120,7 @@ def main():
         elif command.lower() == 'display':
             display(books)
         elif command.lower() == 'exit':
+            # 退出时保存通讯录
             with open('q28.db','wb') as f:
                 pickle.dump(books,f)
             break
