1f25f3f3b65ea680601829bd151b24e95a2ed26e
clove <1103898149@qq.com>
2019-01-21 08:20:02 +0800

    m

--- lx/qqzone.py
+++ lx/qqzone.py
@@ -1,47 +1,47 @@
 from selenium import webdriver
 import time,random,requests
 
-# br = webdriver.Firefox()
-# br.get('https://user.qzone.qq.com/46420820/infocenter')
-# time.sleep(5)
-# br.switch_to_frame('login_frame')
-# time.sleep(random.randint(2,6))
-# br.find_element_by_id('switcher_plogin').click()
+br = webdriver.Firefox()
+br.get('https://user.qzone.qq.com/46420820/infocenter')
+time.sleep(5)
+br.switch_to_frame('login_frame')
+time.sleep(random.randint(2,6))
+br.find_element_by_id('switcher_plogin').click()
 
-# time.sleep(random.randint(2,6))
-# br.find_element_by_id('u').send_keys('46420820')
-# time.sleep(random.randint(2,6))
-# br.find_element_by_id('p').send_keys('87937339')
-# time.sleep(random.randint(2,6))
-# br.find_element_by_id('login_button').click()
-# time.sleep(random.randint(2,6))
-# br.switch_to_default_content()
+time.sleep(random.randint(2,6))
+br.find_element_by_id('u').send_keys('46420820')
+time.sleep(random.randint(2,6))
+br.find_element_by_id('p').send_keys('87937339')
+time.sleep(random.randint(2,6))
+br.find_element_by_id('login_button').click()
+time.sleep(random.randint(2,6))
+br.switch_to_default_content()
 
-# success = input('pause')
+success = input('pause')
 
-# br.find_element_by_xpath("//a[@title='日志']").click()
-# time.sleep(random.randint(2,6))
-# br.switch_to_frame('tblog')
-# time.sleep(random.randint(2,6))
-# log_urls = []
+br.find_element_by_xpath("//a[@title='日志']").click()
+time.sleep(random.randint(2,6))
+br.switch_to_frame('tblog')
+time.sleep(random.randint(2,6))
+log_urls = []
 
-# while True:
-#     time.sleep(random.randint(1,2))
-#     logs = br.find_element_by_id('listArea').find_elements_by_tag_name('a')
-#     for log in logs:
-#         url = log.get_attribute('href')
-#         if url.startswith('http'):
-#             log_urls.append(url)
-#     try:
-#         br.find_element_by_partial_link_text('下一页').click()
-#     except:
-#         break
+while True:
+    time.sleep(random.randint(1,2))
+    logs = br.find_element_by_id('listArea').find_elements_by_tag_name('a')
+    for log in logs:
+        url = log.get_attribute('href')
+        if url.startswith('http'):
+            log_urls.append(url)
+    try:
+        br.find_element_by_partial_link_text('下一页').click()
+    except:
+        break
 
-# log_urls = list(set(log_urls))
-# with open('url.txt','w') as f:
-#     for url in log_urls:
-#         f.write(url)
-#         f.write('\n')
+log_urls = list(set(log_urls))
+with open('url.txt','w') as f:
+    for url in log_urls:
+        f.write(url)
+        f.write('\n')
 
 
 import re
@@ -50,13 +50,14 @@ def validateTitle(title):
     rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
     new_title = re.sub(rstr, "_", title)  # 替换为下划线
     return new_title
+    
+# 以下为从文件获取保存的所有日志URL，逐条获取
+# with open('url.txt','r') as f:
+#     log_urls = f.read()
+# log_urls = log_urls.split('\n')
+# log_urls = [u.strip() for u in log_urls if u.strip()]
 
-with open('url.txt','r') as f:
-    log_urls = f.read()
-log_urls = log_urls.split('\n')
-log_urls = [u.strip() for u in log_urls if u.strip()]
-
-br = webdriver.Firefox()
+# br = webdriver.Firefox()
 
 failed_urls = []
 
