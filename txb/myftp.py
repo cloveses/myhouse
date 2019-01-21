f9ea621d9ec5a7a5e0b07922923282518b9e9a80
djx <1181564186@qq.com>
2019-01-21 20:49:02 +0800

    e

--- txb/myftp.py
+++ txb/myftp.py
@@ -6,9 +6,10 @@ class MyFtp:
         self.server = server
         self.user = user
         self.passwd = passwd
+        self.rpath_depth = 0
         self.ftp = None
-        self.connect()
 
+    # 连接服务器函数
     def connect(self):
         for i in range(2):
             try:
@@ -18,30 +19,35 @@ class MyFtp:
             except:
                 print('times %d' % (i+1))
 
+    #获取指定路径的所有路径名称列表
     def get_pathes(self,path):
         paths = []
         while True:
             pathlst = os.path.split(path)
             if  not pathlst[0] or not pathlst[-1] or pathlst == '.':
-                paths.append(pathlst[-1])
                 break
             paths.append(pathlst[-1])
             path = pathlst[0]
         return paths[::-1]
 
+
+    # 测试路径名是否在服务器返回的路径列表中
     def exists(self,dlst,directory):
         for d in dlst:
             if d.startswith('d') and d.endswith(directory):
                 return True
 
-
-    def enter_dir(self,paths):
-        res = self.ftp.cwd('/')
+    # 进行要上传的FTP服务器目录
+    def enter_dir(self, paths, dest_dir):
+        # 首先切换到服务器的目标根目录
+        res = self.ftp.cwd(dest_dir)
         if not res.startswith('250'):
             print('change directory failed!')
             return
         paths = self.get_pathes(paths)
-        print(paths)
+        # print(paths)
+        paths = paths[self.rpath_depth:]
+        # 逐级进行目录目录，不存在，则创建
         for path in paths:
             res = self.ftp.nlst()
             if not self.exists(res, path):
@@ -54,17 +60,29 @@ class MyFtp:
                 return
         return True
 
-    def upload(self, path):
-        if not self.ftp:
-            print('Link error!')
-            return
+    # 上传文件函数
+    def upload(self, path, dest_dir):
+        #测试本地目录是否存在，并转为绝对路径
         if not os.path.exists(path):
             print('directory is not exists!')
             return
+        else:
+            path = os.path.abspath(path)
+        self.rpath_depth = len(self.get_pathes(path)) - 1
+        if self.rpath_depth < 0:
+            self.rpath_depth = 0
+        # 获取本地目录中所有文件
         all_files = os.walk(path)
+        #调用函数连接FTP服务器
+        self.connect()
+        if not self.ftp:
+            print('Link error!')
+            return
+        # 逐目录上传文件
         for root,dirs,files in all_files:
             if files:
-                self.enter_dir(root)
+                # 进行目标路径
+                self.enter_dir(root, dest_dir)
                 for f in files:
                     print(root)
                     fp = open(os.path.join(root,f),'rb')
@@ -72,7 +90,9 @@ class MyFtp:
                     if not res.startswith('226'):
                         print('Put file Failed!')
                     fp.close()
+        self.ftp.quit()
+
 
 if __name__ == '__main__':
     ftp = MyFtp('127.0.0.1', 'anonymous', '')
-    ftp.upload('aa')
+    ftp.upload('D:\\lx\\qx', '/ass')
