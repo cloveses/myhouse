62db574e7792f0aeb9e3bd3718dd8522802db22c
djx <1181564186@qq.com>
2019-02-02 23:26:56 +0800

    e

---
+++ crawbook/models.py
@@ -0,0 +1,18 @@
+import os
+from pony.orm import *
+
+db = Database()
+
+class Book(db.Entity):
+    title = Required(str)
+    writer = Required(str)
+    published = Required(int)
+    pages = Required(int)
+    downloads = Required(int)
+    excerpt = Required(LongStr)
+    tags = Required(str)
+
+# set_sql_debug(True)
+filename = os.path.join(os.path.abspath(os.curdir),'my.db')
+db.bind(provider='sqlite', filename=filename, create_db=True)
+db.generate_mapping(create_tables=True)
