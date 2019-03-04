f00b5015944dc922e363ceff93f6b8fc93673caa
djx <cloveses@126.com>
2019-03-04 17:28:47 +0800

    m

--- distri/qq.py
+++ distri/qq.py
@@ -93,7 +93,7 @@ def dijkstra_2():
 def tool(g, shortest, curr_id, src_paths, des_paths):
     curr_vert = g.getVertex(curr_id)
     if not curr_vert:
-        continue
+        return shortest, curr_id
     # update des_paths
     dists = {vid:dist+shortest for vid, dist in 
                 curr_vert.connectedTo.items() if vid not in src_paths}
@@ -125,6 +125,21 @@ def tool(g, shortest, curr_id, src_paths, des_paths):
 
     return shortest, curr_id
 
+def get_min_path(flag, src_paths, des_paths):
+    min_dist = -1
+    min_mid = None
+    for mid in flag:
+        dist = src_paths[mid][0] + des_paths[mid][0]
+        if min_dist != -1:
+            if dist < min_dist:
+                min_dist = dist
+                min_mid = mid
+        else:
+            min_dist = dist
+            min_mid = mid
+    return min_mid, min_dist
+
+
 def dijkstra_3():
     g, info = build_from_file()
     src, des = info
@@ -134,29 +149,47 @@ def dijkstra_3():
     shortest_before = 0
     curr_id_before = src
 
-    src_paths_after = {des:[0,[]], }
+    src_paths_after = {des:[5,[]], }
     des_paths_after = {vert.getName():[-1,[]] for vert in g.vertList.values() 
                 if vert.getName() != des }
     shortest_after = 0
     curr_id_after = des
 
-    while not (set(src_paths_before.keys()) & set(src_paths_after)):
+    # while minpaths have public element:quit
+    flag = None
+    while not flag:
         shortest_before, curr_id_before = tool(g, shortest_before, 
             curr_id_before, src_paths_before, des_paths_before)
 
         shortest_after, curr_id_after = tool(g, shortest_after, 
             curr_id_after, src_paths_after, des_paths_after)
 
+        flag = set(src_paths_before.keys()) & set(src_paths_after)
+
     #     print('src_paths',src_paths)
     # print(src,end=' ')
     # for p in src_paths[des][-1]:
     #     print(p, end=' ')
+    # print(flag)
+    # print(src_paths_before)
+    # print(src_paths_after)
+
+    # get min path from flag(middle vertex)
+    mid,dist = get_min_path(flag, src_paths_before, src_paths_after)
+    print(src, end=' ')
+    for vert in src_paths_before[mid][1]:
+        print(vert, end=' ')
+    for vert in src_paths_after[mid][1][1:]:
+        print(vert, end='')
+    print(des, end='')
 
 
 
 if __name__ == '__main__':
 
-    print('initial...')
-    dijkstra()
-    print('idea 1')
-    dijkstra_2()
+    # print('initial...')
+    # dijkstra()
+    # print('idea 1')
+    # dijkstra_2()
+    # print('idea 2')
+    dijkstra_3()
