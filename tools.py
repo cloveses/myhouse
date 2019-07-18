e56fd6fac05977c51d4d29167c67dc60be0cfae1
djx <1181564186@qq.com>
2019-07-19 07:45:14 +0800

    e

--- tools.py
+++ tools.py
@@ -1,11 +1,18 @@
 import xlrd
 import win32clipboard as w
+import win32con
+
+def get_clip():
+    w.OpenClipboard()
+    txt = w.GetClipboardData(win32con.CF_UNICODETEXT)
+    w.CloseClipboard()
+    return txt
 
 def set_clip(data):
-     w.OpenClipboard()
-     w.EmptyClipboard()
-     w.SetClipboardText(data)
-     w.CloseClipboard()
+    w.OpenClipboard()
+    w.EmptyClipboard()
+    w.SetClipboardText(data)
+    w.CloseClipboard()
 
 def get_file_datas(filename,row_deal_function=None,grid_end=0,start_row=1):
     """start_row＝1 有一行标题行；gred_end=1 末尾行不导入"""
@@ -22,12 +29,22 @@ def get_file_datas(filename,row_deal_function=None,grid_end=0,start_row=1):
 
 if __name__ == '__main__':
     datas = get_file_datas('data.xlsx')
-    for data in datas:
-        c = None
-        if c == 'q':
+    while True:
+        command = input(' q ? ')
+        if command == 'q':
             break
-        for d in data:
-            set_clip(d)
-            c = input()
+
+        idcode = get_clip()
+        if not idcode:
+            print('Do not find idcode from clip!')
+        else:
+            
+        for data in datas:
+            c = None
             if c == 'q':
                 break
+            for d in data:
+                set_clip(d)
+                c = input()
+                if c == 'q':
+                    break
